7abccab301dfd207f74249213a573d46a0a98b13
Ea Rist <earis@greeny.cs.tlu.ee>
2015-10-23 16:54:01 +0300

    edit files, still errors in funk2 and editk

--- edit_functionsk.php
+++ edit_functionsk.php
@@ -1,1 +1,48 @@
-ef
+<?php
+	require_once("../config_global.php");
+	$database = "if15_earis_3";
+	
+	function getEditData($edit_id){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT raviminimi, hinnang, kommentaar FROM ravimid WHERE id=? AND deleted IS NULL");
+		$stmt->bind_param("i", $edit_id);	//bind_param asendab küsimärgid
+		$stmt->bind_result($raviminimi, $hinnang, $kommentaar);
+		$stmt->execute();
+		
+		$review = new StdClass();
+		
+		//kas sain ühe rea andmeid kätte
+		//$stmt->fetch() annab ühe rea andmeid
+		if($stmt->fetch()){
+			//sain
+			
+			$review->raviminimi =$raviminimi;
+			$review->hinnang=$hinnang;
+			$review->kommentaar=$kommentaar;
+			
+			
+		}else{
+			header("Location:tablek.php");	 //See viib tagasi table esilehele
+		}
+		return $review;
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	function updateCar($id, $raviminimi, $hinnang, $kommentaar){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE ravimid SET raviminimi=?, hinnang=?, kommentaar=? WHERE id=?");
+		$stmt->bind_param("sssi", $raviminimi, $hinnang, $kommentaar, $id);
+		if($stmt->execute()){
+			//sai kustutatud, kustutame aadressirea tühjaks
+			//header("Location: table.php");
+			
+		}
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+
+?>
--- editk.php
+++ editk.php
@@ -1,1 +1,43 @@
-edit
+<?php
+
+	require_once("edit_functionsk.php");
+	
+	if(isset($_POST["update_review"])){
+		//vajuta Salvesta nuppu
+		//numberplate ja color tulevad vormist, aga id aadressirealt
+		//aga id varjatud väljast
+		updateReview($_POST["id"], $_POST["raviminimi"], $_POST["hinnang"], $_POST["kommentaar"]);
+	}
+	
+	//aadressireal on=edit_id siis trükin välja selle väärtuse
+	if(isset($_GET["edit_id"])){
+		echo $_GET["edit_id"];
+		
+		//id oli aadressireal
+		//tahaks ühte rida kõige uuemaid andmeid, kus id on $_GET["edit_id"]
+		
+		$review = getEditData($_GET["edit_id"]);
+		var_dump($review);
+		
+	}else{
+		//ei olnud aadressireal
+		echo "VIGA";
+		header("Location:tablek.php");
+		
+	}
+	
+	
+
+?>
+
+<h2>Muuda ravimi hinnagut</h2>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+		<input type="hidden" name="id" value="<?=$_GET["edit_id"];?>">
+		<label for ="raviminimi">Ravimi nimi</label><br>
+		<input id="raviminimi" name="raviminimi" type="text" value="<?=$review->raviminimi;?>"><br><br>
+		<label for ="hinnang">Hinnang</label><br>
+		<input id="hinnang" name="hinnang" type="text" value="<?=$review->hinnang;?>"> <br><br>
+		<label for ="kommentaar">Kommentaar</label><br>
+		<input id="kommentaar" name="kommentaar" type="text" value="<?=$review->kommentaar;?>"> <br><br>
+		<input type="submit" name="update_review" value="Salvesta"><br>
+		</form>	
