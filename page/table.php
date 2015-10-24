288cec1df5fcaa33cc2ae9a985365d63b6335831
Martin Viidik <martvii@greeny.cs.tlu.ee>
2015-10-24 20:59:36 +0300

    added table.php

---
+++ page/table.php
@@ -0,0 +1,76 @@
+<?php	
+
+	require_once("functions.php");
+	
+	// kas kustutame midagi ??
+	// ?delete=vastav id mida kustutada on aadressireal
+	if(isset($_GET["delete"])){
+		
+		echo "Deletingid".$_GET["delete"];
+		// käivitan funktsiooni, saadan kaasa id !!
+		deleteThread($_GET["delete"]);
+	}
+	
+	// salvestan andmebaasi uuendused
+	if(isset($_POST["save"])){
+		
+		updateThread($_POST["id"], $_POST["thread"], $_POST["post"]);
+	}
+
+	//käivitan funktsiooni
+	$array_of_threads = getThreadData();
+	
+	// trükin välja esimese auto
+	//echo $array_of_threads[0]->id." ".$array_of_threads[0]->thread;
+?>
+
+<h2>Threads</h2>
+<table border=1 >
+	<tr>
+		<th>id</th>
+		<th>user id</th>
+		<th>Title</th>
+		<th>Post</th>
+		<th>Delete</th>
+		<th>Edit</th>
+	</tr>
+	
+	<?php
+		// Trükime välja read
+		// massiivi pikkus count()
+		for($i = 0; $i < count($array_of_threads); $i++){
+			//echo $array_of_threads[$i]->id;
+			
+			// kasutaja tahab muuta seda rida
+			if(isset($_GET["edit"]) && $array_of_threads[$i]->id == $_GET["edit"]){
+				
+				echo "<tr>";
+				echo "<form action= 'table.php' method='post'>";
+				echo "<input type='hidden' name='id' value='".$array_of_threads[$i]->id."'>";
+				echo "<td>".$array_of_threads[$i]->id."</td>";
+				echo "<td>".$array_of_threads[$i]->user_id."</td>";
+				echo "<td><input name='plate_number' value='".$array_of_threads[$i]->thread."'></td>";
+				echo "<td><input name='color'value='".$array_of_threads[$i]->post."'></td>";
+				echo "<td><a href='table.php'>cancel</a></td>";
+				echo "<td><input type='submit' name='save'></td>";
+				echo "</form>";
+				echo "</tr>";
+				
+			}else{
+					
+				echo "<tr>";
+				echo "<td>".$array_of_threads[$i]->id."</td>";
+				echo "<td>".$array_of_threads[$i]->user_id."</td>";
+				echo "<td>".$array_of_threads[$i]->thread."</td>";
+				echo "<td>".$array_of_threads[$i]->post."</td>";
+				echo "<td><a href='?delete=".$array_of_threads[$i]->id."'>X</a></td>";
+				echo "<td><a href='?edit=".$array_of_threads[$i]->id."'>edit</a></td>";
+				echo "<td><a href='edit.php?edit_id=".$array_of_threads[$i]->id."'>edit</a></td>";
+				echo "</tr>";
+			}
+			
+		}
+	
+	?>
+
+</table>
