6f3af9ae05ce7e0528f7ea99fa6b56e66832405d
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-10-25 17:05:05 +0200

    liigutasin configi dirist välja

--- hidden/functions.php
+++ hidden/functions.php
@@ -1,5 +1,5 @@
 <?php
-	require_once("config.php");
+	require_once("../config.php");
 	$database = "if15_hendval";
 	
 	session_start();
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-	require_once("hidden/config.php");
+	require_once("../config.php");
 	require_once("hidden/functions.php");
 	$database = "if15_hendval";
 
