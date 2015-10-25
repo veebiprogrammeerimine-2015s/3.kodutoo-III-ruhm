1b0d569199f5277cb8fcabf2d021d5b2c7fa5cfb
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2015-10-25 21:35:13 +0200

    Still some errors

--- Page/poststable.php
+++ Page/poststable.php
@@ -8,17 +8,25 @@
 		
 		echo "Kustutame id ".$_GET["delete"];
 		
-		deletePost($_GET["delete"]);
+		deletePosts($_GET["delete"]);
 		
 	}
 	if(isset($_POST["save"])){
 		
-		updatePosts($_POST["id"], $_POST["plate_number"], $_POST["color"]);
+		updatePosts($_POST["id"], $_POST["post"]);
 		
 	}
-	$array_of_posts=getPostData();
+	$keyword="";
+	if(isset($_GET["keyword"])){
+		
+		$keyword=$_GET["keyword"];
+		$array_of_posts=getPostsData($keyword);
+		
+	}else{
+
+	$array_of_posts=getPostsData();
 	
-	
+	}
 
 ?>
 
@@ -33,9 +41,9 @@
 	
 	</tr>
 <?php
-		//trükime välja read
+		
 	for($i=0; $i< count($array_of_posts) ; $i++){
-			//echo $array_of_cars[$i]->id;
+			
 			
 		if(isset($_GET["edit"]) && $array_of_posts[$i]->id == $_GET["edit"]){
 				
@@ -44,7 +52,7 @@
 			echo"<input type='hidden' name='id' value='".$posts_array[$i]->id."'>";
 			echo"<td>".$array_of_posts[$i]->id."</td>";
 			echo"<td>".$array_of_posts[$i]->user_id."</td>";
-			echo"<td><input name='plate_number' value='".$array_of_posts[$i]->post."'></td>";
+			echo"<td><input name='post' value='".$array_of_posts[$i]->post."'></td>";
 			echo"<td><a href='table.php'>cancel<a></td>";
 			echo"<td><input type='submit' name='save'></td>";
 			echo"</form>";
