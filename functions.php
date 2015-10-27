3cb551bb44a82b0f62dd883050ef911742810cb0
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2015-10-27 20:33:23 +0200

    sain sisselogimise tööle

---
+++ functions.php
@@ -0,0 +1,60 @@
+<?php
+	// functions.php
+	// siia tulevd funktsioonid, kõik mis seotud AB'ga
+	
+	// Loon AB'i ühenduse
+	require_once("../configGlobal.php");
+	$database = "if15_toomloo_3";
+	
+	// tekitatakse sessioon, mida hoitakse serveris
+	// kõik session muutujad on kättesaadavad kuni viimase brauseriakna sulgemiseni
+	session_start();
+	
+	function register($create_email, $hash){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+		$stmt->bind_param("ss", $create_email, $hash);
+		$stmt->execute();
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	function cleanInput($data) {
+  	  $data = trim($data);
+  	  $data = stripslashes($data);
+  	  $data = htmlspecialchars($data);
+  	  return $data;
+    }
+	
+	function login($email, $hash){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+		$stmt->bind_param("ss", $email, $hash);
+		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt->execute();
+		//Kontrollin kas tulemusi leiti
+		if($stmt->fetch()){
+			// ab'i oli midagi
+			echo "Email ja parool õiged, kasutaja id=".$id_from_db;
+			
+			// tekitan sessiooni muutujad
+			$_SESSION["logged_in_user_id"] = $id_from_db;
+			$_SESSION["logged_in_user_email"] = $email_from_db;
+			
+			// suunan data.php lehele
+			header("Location: data.php");
+			
+		}else{
+			// ei leidnud
+			echo "Wrong credentials!";
+		}
+				
+		$stmt->close();
+		
+		$mysqli->close();
+		
+	}
+	
+
+?>
